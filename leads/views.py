118946d8ddf7cdb4e7883427adadf006a7d8a439
Max Arkell <maxarkell@icloud.com>
2024-02-29 22:37:32 +0000

    change home page to html

--- leads/views.py
+++ leads/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-    return HttpResponse("Hello World!")
+    #return HttpResponse("Hello World!")
+    return render(request, "leads/home_page.html")
