97cebc18ca6db87465202fd9f43565549d717514
Max Arkell <maxarkell@icloud.com>
2024-03-05 16:51:19 +0000

    set up href tags in lead list Create links in leads page. Create views function to send unique pk

--- leads/views.py
+++ leads/views.py
@@ -8,3 +8,9 @@ def leads_list(request):
         "leads" : leads
     }
     return render(request, "leads/lead_list.html", context)
+
+def lead_detail(request, pk):
+    print(pk)
+    lead = Lead.objects.get(id=pk)
+    print(lead)
+    return HttpResponse("Here is the detail view")
