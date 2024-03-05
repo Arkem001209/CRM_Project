cd785848f195e9e20cceb942a29ea3ea5c03f6d4
Max Arkell <maxarkell@icloud.com>
2024-03-05 16:56:35 +0000

    create html page for front page urls

--- leads/views.py
+++ leads/views.py
@@ -10,7 +10,8 @@ def leads_list(request):
     return render(request, "leads/lead_list.html", context)
 
 def lead_detail(request, pk):
-    print(pk)
     lead = Lead.objects.get(id=pk)
-    print(lead)
-    return HttpResponse("Here is the detail view")
+    context = {
+        "lead" : lead
+    }
+    return render(request, "leads/lead_detail.html", context)
